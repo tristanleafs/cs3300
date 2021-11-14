443d70bb50d040bda8b42bc0b9266c1160675ca2
Tristan <tristan.lee@theleehome.org>
2021-11-14 01:24:05 +0000

    checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
-Rails.application.routes.draw do
-  resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+# Rails.application.routes.draw do
+#   devise_for :users
+#   resources :projects
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
 
 #adding root
 
 Rails.application.routes.draw do
+  devise_for :users
   root "projects#index"
   resources :projects
 end
