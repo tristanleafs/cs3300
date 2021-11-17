19a6b08bb21b77ce0b6d15cb5238989acfc78048
Tristan Lee <tlee2@uccs.edu>
2021-11-17 02:26:10 +0000

    Finally got rspec testing to work with devise

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
-
 RSpec.describe ProjectsController, type: :controller do
-
-
   context "GET #index" do
     it "returns a success response" do
       get :index
@@ -19,68 +16,63 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to be_success
     end
   end
-end
 
-RSpec.feature "Visiting the homepage", type: :feature do
-  scenario "The visitor should see projects" do
-    visit root_path
-    expect(page).to have_text("Projects")
-  end
-end
-
-RSpec.feature "Projects", type: :feature do
-  context "Create new project" do
+  context "GET #new" do
     login_user
-    before(:each) do
-      visit new_project_path
-      within("form") do
-        fill_in "Title", with: "Test title"
-      end
-    end
-
-    scenario "should be successful" do
-
-      fill_in "Description", with: "Test description"
-      click_button "Create Project"
-      expect(page).to have_content("Project was successfully created")
-    end
-
-    scenario "should fail" do
-      click_button "Create Project"
-      expect(page).to have_content("Description can't be blank")
+    it "returns a success response" do
+      get :new
+      expect(response).to be_success
     end
   end
 
-  context "Update project" do
-    let(:project) { Project.create(title: "Test title", description: "Test content") }
-    before(:each) do
-      visit edit_project_path(project)
-    end
-
-    scenario "should be successful" do
-      within("form") do
-        fill_in "Description", with: "New description content"
-      end
-      click_button "Update Project"
-      expect(page).to have_content("Project was successfully updated")
-    end
-
-    scenario "should fail" do
-      within("form") do
-        fill_in "Description", with: ""
-      end
-      click_button "Update Project"
-      expect(page).to have_content("Description can't be blank")
+  context "POST #projects" do
+    login_user
+    it "returns a success response" do
+      post :create, :params => { :project => { :title => "Any Name" , :description => "Any Name" } }
+      
+      #expect(response).to be_success
+      expect(Project.count).to eq(1)
     end
   end
 
-  context "Remove existing project" do
-    let!(:project) { Project.create(title: "Test title", description: "Test content") }
-    scenario "remove project" do
-      visit projects_path
-      click_link "Destroy"
-      expect(page).to have_content("Project was successfully destroyed")
+  context "POST #projects" do
+  login_user
+  it "returns a bad response" do
+    post :create, :params => { :project => { :title => "" , :description => "Any Name" } }
+    
+    #expect(response).to be_success
+    expect(Project.count).to eq(0)
+  end
+end
+
+  context "PATCH #projects/1" do
+    login_user
+    let!(:project) { Project.create(title: "Test title", description: "Test") }
+    it "returns a success response" do
+      patch :update, :params => { :id => project, :project => { :title => "Test title",  :description => "Test" }} #project: project, body: "new body"}
+      expect(Project.first.title).to eq "Test title"
+    end
+  end
+
+  context "PATCH #projects/1" do
+  login_user
+  let!(:project) { Project.create(title: "Test title", description: "Test") }
+  it "returns a failure response" do
+    patch :update, :params => { :id => project, :project => { :title => "", :description => "Test" }} #project: project, body: "new body"}
+    expect(Project.first.description).to eq "Test"
+  end
+end
+
+  context "DELETE #projects/1" do
+    login_user
+    let!(:project) { Project.create(title: "Test title", description: "Test description") }
+    it "Project was deleted" do
+      expect(Project.count).to eq(1)
+      delete :destroy, :params => { :id => project }
       expect(Project.count).to eq(0)
     end
+    it "Project was deleted" do
+    end
   end
+
 end
